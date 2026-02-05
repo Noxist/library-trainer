d98ae3e869a83dd94eb79d49c46f78a396616221
Nox <leandro.aeschbacher77@gmail.com>
2026-02-05 10:24:57 +0000

    fix: switch to relative paths and add nojekyll

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // WICHTIG: Hier muss dein Repo-Name stehen, z.B. '/library-trainer/'
-  base: '/library-trainer/', 
+  base: './', 
 })
